85120318aeaffdc891123c2dd8ee3defbac6de70
Farid <faridmanzo@gmail.com>
2015-09-02 12:47:02 -0400

    ajout de la querystring dans la page index

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 // Blqblq	
 require_once 'data/_data.php';
 $emp_id = 102; // Employé "par défaut"
-
+// TODO: Lire et mettre à jour l'id de l'employé en Query string
+//var_dump($_GET);
+if(isset($_GET[PARAM_EMP_ID])) {
+	$emp_id = $_GET[PARAM_EMP_ID];
+}
 
 $liste_employes = get_employes(); // Liste des noms de tous les employés
 $emp_data = $liste_employes[$emp_id];
